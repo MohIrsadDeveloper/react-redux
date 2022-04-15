2cc0241ebd7183052236ef1ddcd03f8cfe48c66b
Moh Irsad Idrisi <idrisimohdirshad@gmail.com>
2022-04-15 10:48:05 +0530

    react-redux overview

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 import React from 'react'
+import { createStore } from 'redux';
+
+const rootReducer = (oldState={value : "Abdul"}, action) => {
+    let newState = oldState;
+    if (action.type === "abc") {
+        newState.value = "Abdul Bari Idrisi";
+        return newState;
+    } else if (action.type === "xyz") {
+        newState.value = "Asma"
+        return newState;
+    } else {
+        return newState;
+    }
+}
+
+
+const store = createStore(rootReducer)
+console.log(store);
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({type : "abc"})
+store.dispatch({type : "abc"})
+store.dispatch({type : "abc"})
+store.dispatch({type : "xyz"})
+store.dispatch({type : "xyz"})
+store.dispatch({type : "xyz"})
 
 const App = () => {
   return (
